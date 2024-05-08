77ef1bf80dacc3bba2cbe7d3e97cb36065f79866
Edmund <matrim.eg@gmail.com>
2024-05-08 22:02:46 +0000

    Added test

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
-	tr := NewTCPTransport()
+	tr := NewTCPTransport(listenAddr)
 
 	assert.Equal(t, tr.listenAddress, listenAddr)
 }
