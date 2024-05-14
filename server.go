461b73a8c1b304c5522da6f7d0d0dee7ad78c7ed
Edmund <matrim.eg@gmail.com>
2024-05-14 15:49:08 +0000

    3h47m

--- server.go
+++ server.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/vssn/dist-fs-go/p2p"
+import (
+	"fmt"
+
+	"github.com/vssn/dist-fs-go/p2p"
+)
 
 type FileServerOpts struct {
 	ListenAddr        string
@@ -12,7 +16,8 @@ type FileServerOpts struct {
 
 type FileServer struct {
 	FileServerOpts
-	store *Store
+	store  *Store
+	quitch chan struct{}
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
@@ -24,10 +29,26 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 	return &FileServer{
 		FileServerOpts: opts,
 		store:          NewStore(storeopts),
+		quitch:         make(chan struct{}),
 	}
 }
 
-func (s *FileServerOpts) Start() error {
+func (s *FileServer) Stop() {
+	close(s.quitch)
+}
+
+func (s *FileServer) loop() {
+	for {
+		select {
+		case msg := <-s.Transport.Consume():
+			fmt.Println(msg)
+		case <-s.quitch:
+			return
+		}
+	}
+}
+
+func (s *FileServer) Start() error {
 	if err := s.Transport.ListenAndAccept(); err != nil {
 		return err
 	}
