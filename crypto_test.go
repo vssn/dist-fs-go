a6fbab1856a53a2968729c76107f7f0b61d7db19
Edmund <matrim.eg@gmail.com>
2024-05-31 19:11:27 +0000

    8h26m crypto.go

---
+++ crypto_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCopyEncryptDecrypt(t *testing.T) {
+	payload := "Foo not bar"
+	src := bytes.NewReader([]byte(payload))
+
+	dst := new(bytes.Buffer)
+	key := newEncryptionKey()
+	_, err := copyEncrypt(key, src, dst)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	out := new(bytes.Buffer)
+	if _, err := copyDecrypt(key, dst, out); err != nil {
+		t.Error(err)
+	}
+
+	if out.String() != payload {
+		t.Errorf("decryption failed")
+	}
+}
